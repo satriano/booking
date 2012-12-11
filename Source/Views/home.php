e5cf2059eefc493578b1910a0559655cb1fc949b
Jason Edwards <prenk10@gmail.com>
2012-12-11 15:46:40 +0000

    Changed primary button link to reflect new module changes.

--- Source/Views/home.php
+++ Source/Views/home.php
@@ -3,7 +3,7 @@
       <h1>Welcome</h1>
       <p>This is the Lowry Equipment Booking system.</p>
       <p>
-        <a class="btn btn-primary btn-large" href="/index.php?module=booking&action=bookingform">
+        <a class="btn btn-primary btn-large" href="/index.php?module=booking&action=new">
           Make a Booking &raquo;
         </a>
       </p>
