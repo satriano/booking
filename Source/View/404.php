687ca398550031608b3c322b2f1f73832987c394
Jason Edwards <prenk10@gmail.com>
2012-12-11 13:16:41 +0000

    Updated 404 page to have same header and footer as rest of site

--- Source/View/404.php
+++ Source/View/404.php
@@ -1,7 +1,9 @@
-    <div class="hero-unit">
+<?php include ("View/header.php"); ?>
+<div class="hero-unit">
       <h1>404 - File not found</h1>
       <p><?php echo($_GET['action']);?> does not exist.</p>
       <p>
         Please use the navigation menu above to choose an appropriate action.
       </p>
     </div>
+<?php include ("View/Footer.php"); ?>
